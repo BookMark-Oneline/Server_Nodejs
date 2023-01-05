739c8c884d9199058d35eac04b9416ab2ef54013
Dawon00 <da6921@naver.com>
2023-01-06 04:05:03 +0900

    npm install body-parser

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 // DB 연결
 // var db = require('./lib/db')
 

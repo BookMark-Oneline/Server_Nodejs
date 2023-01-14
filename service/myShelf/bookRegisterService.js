4474fea445ed0557c2e7b96d8f7fd59249ccbc7b
E ON <ykiki5778@inha.edu>
2023-01-15 06:47:46 +0900

    update : 책 등록 API

--- service/myShelf/bookRegisterService.js
+++ service/myShelf/bookRegisterService.js
@@ -5,13 +5,19 @@ const baseResponse = require('../../config/baseResponse');
 
 
 // Service: Create, Update, Delete 비즈니스 로직 처리
-exports.registerBook = async (user_id, title, author, publisher, img_url) => {
+exports.registerBook = async (user_id, book_id, title, img_url, author, publisher, isbn) => {
 
-    const registerBookInfoParams = [user_id, title, author, publisher, img_url];
-    const connection = await pool.getConnection(async (conn) => conn);
-    const bookReigsterResult = await bookDao.registerBookInfo(connection,registerBookInfoParams);
-    
-    connection.release();
+    // 여기서 user_id 제거?
+    const registerBookInfoParams = [parseInt(book_id), title, img_url, author, publisher, isbn];
+    const Bookconnection = await pool.getConnection(async (conn) => conn);
+    const bookReigsterResult = await bookDao.registerBookInfo(Bookconnection,registerBookInfoParams);
+
+
+    const registerBookShelfParams = [parseInt(user_id), parseInt(book_id)];
+    const Shelfconnection = await pool.getConnection(async (conn) => conn);
+    const bookReigsterResult2 = await bookDao.registerBookShelf(Shelfconnection,registerBookShelfParams);
+
+    Bookconnection.release();
 
     return response(baseResponse.SUCCESS);
 

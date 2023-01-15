ddd718eebbd63f35bd2d371592ea506d3c88b06b
Dawon00 <da6921@naver.com>
2023-01-15 16:17:35 +0900

    update: 책 등록시 book_id auto_increment

--- service/myShelf/bookRegisterService.js
+++ service/myShelf/bookRegisterService.js
@@ -1,26 +1,36 @@
 const pool = require("../../config/database");
 const bookDao = require("../../dao/myShelf/bookDao");
 const { errResponse, response } = require("../../config/response");
-const baseResponse = require('../../config/baseResponse');
-
+const baseResponse = require("../../config/baseResponse");
 
 // Service: Create, Update, Delete 비즈니스 로직 처리
-exports.registerBook = async (user_id, book_id, title, img_url, author, publisher, isbn) => {
+exports.registerBook = async (
+  user_id,
+  title,
+  img_url,
+  author,
+  publisher,
+  isbn
+) => {
+  // const registerBookInfoParams = [title, img_url, author, publisher, isbn];
+  const Bookconnection = await pool.getConnection(async (conn) => conn);
+  const bookReigsterResult = await bookDao.registerBookInfo(Bookconnection, [
+    user_id, // 추가
+    title,
+    img_url,
+    author,
+    publisher,
+    isbn,
+  ]);
 
-    // 여기서 user_id 제거?
-    const registerBookInfoParams = [parseInt(book_id), title, img_url, author, publisher, isbn];
-    const Bookconnection = await pool.getConnection(async (conn) => conn);
-    const bookReigsterResult = await bookDao.registerBookInfo(Bookconnection,registerBookInfoParams);
+  //const registerBookShelfParams = [parseInt(user_id), parseInt(book_id)];
+  //const Shelfconnection = await pool.getConnection(async (conn) => conn);
+  //const bookReigsterResult2 = await bookDao.registerBookShelf(Shelfconnection, [
+  //  parseInt(user_id),
+  //  parseInt(book_id),
+  //]);
 
+  Bookconnection.release();
 
-    const registerBookShelfParams = [parseInt(user_id), parseInt(book_id)];
-    const Shelfconnection = await pool.getConnection(async (conn) => conn);
-    const bookReigsterResult2 = await bookDao.registerBookShelf(Shelfconnection,registerBookShelfParams);
-
-    Bookconnection.release();
-
-    return response(baseResponse.SUCCESS);
-
-    
-}
-
+  return response(baseResponse.SUCCESS);
+};

62e8b9a653df7c217aa5c55f3cb2cf14651f0cb1
bik1111 <bik1111@khu.ac.kr>
2023-01-18 02:37:14 +0900

    fix : PORT, DB_HOST, 수정

--- provider/myShelf/searchBookProvider.js
+++ provider/myShelf/searchBookProvider.js
@@ -1,11 +1,9 @@
-const pool = require('../../config/database')
-const { selectUserId } = require('../../dao/myShelf/UserDao')
+const pool = require("../../config/database");
+const { selectUserId } = require("../../dao/myShelf/userDao");
 
-
-exports.retrieveUserId = async(user_id) => {
-        const connection = await pool.getConnection(async conn => conn);
-        const userIdResult = await selectUserId(connection, user_id);
-        connection.release();
-        return userIdResult[0];
-
+exports.retrieveUserId = async (user_id) => {
+  const connection = await pool.getConnection(async (conn) => conn);
+  const userIdResult = await selectUserId(connection, user_id);
+  connection.release();
+  return userIdResult[0];
 };

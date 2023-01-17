f31c406eaf9db282abed54af5fa3cf7cf16e72bb
bik1111 <bik1111@khu.ac.kr>
2023-01-18 00:55:00 +0900

    fix:userDao delete

--- dao/myShelf/userDao.js
+++
@@ -1,5 +0,0 @@
-module.exports.selectUserId = async(connection, user_id) => {
-        const selectUserInfoQuery = `SELECT user_id FROM UserInfo WHERE user_id = ?`;
-        const [userId] =  await connection.query(selectUserInfoQuery, user_id);
-        return userId;
-};

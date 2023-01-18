8c4110966a8fe4a5e8731c9c4bd3da5287cc8170
Jin Cheol <76617139+bik1111@users.noreply.github.com>
2023-01-18 21:00:36 +0900

    Update database.js

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2/promise");
  const config = {
    host: `${process.env.DB_HOST}`,
    user: `${process.env.DB_USER}`,
-   port: `31696`,
+   port: `30118`,
    password: `${process.env.DB_PASS}`,
   database: `${process.env.DB_NAME}`,
  };

345cd4085962ba1fa9a0bcfe2f528d9ac73593a2
Dawon00 <da6921@naver.com>
2023-01-22 21:32:27 +0900

    add: new 'club' folder

--- config/database.js
+++ config/database.js
@@ -5,7 +5,7 @@ const mysql = require("mysql2/promise");
  const config = {
    host: `${process.env.DB_HOST}`,
    user: `${process.env.DB_USER}`,
-   port: `3306`,
+   port: `31696`,
    password: `${process.env.DB_PASS}`,
   database: `${process.env.DB_NAME}`,
  };

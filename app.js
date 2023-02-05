3a11fdca1f9defae9725da774bbf25cc9b176ff2
E ON <ykiki5778@inha.edu>
2023-02-05 19:14:58 +0900

    upload: route, controller, provider, service, dao  파일 이름 변경

--- app.js
+++ app.js
@@ -10,12 +10,7 @@ const timerRouter = require("./router/myShelf/timerRoute");
 const bookRegisterRouter = require("./router/myShelf/bookRegisterRouter");
 const bookDeleteRouter = require("./router/myShelf/bookDeleteRouter");
 
-// const userBelongClubRouter = require("./router/club/userBelongRouter");
-// const clubDetailInfoRouter = require("./router/club/clubDetailInfoRouter");
-// const clubMemberRouter = require("./router/club/clubMemberRouter");
-// const clubRequestRouter = require("./router/club/clubRequestRouter");
-// const clubSearchRouter = require("./router/club/clubSearchRouter");
-const postRouter = require("./router/club/postRoute");
+const clubRouter = require("./router/club/clubRoute");
 
 
 const bodyParser = require("body-parser");
@@ -51,11 +46,6 @@ app.use("/timer", timerRouter);
 app.use("/register", bookRegisterRouter);
 app.use("/delete", bookDeleteRouter);
 
-// app.use("/club/user", userBelongClubRouter);  // 사용자(userid)가 속한 책모임을 조회함   -----> 성공
-// app.use("/club", clubDetailInfoRouter);       // 책 모임의 이름, 공지, 게시글 목록을 조회함 -----> 성공
-// app.use("/club/member", clubMemberRouter);    // 책 모임의 소속 회원의 이름, 상태메시지, 현재 상태를 조회함 --> 성공
-// app.use("/club/request", clubRequestRouter);  // 책 모임에 가입요청을 보냄   1-1일차   ---> 왜 안들어갈까요
-// app.use("/club/search", clubSearchRouter);    // 책 모임을 검색하여 조회함   1-2일차   --> 게시글이 하나씩 가져와진다..
-app.use("/club", postRouter)
+app.use("/club", clubRouter)
 
 module.exports = app;
--- controller/club/clubController.js
+++ controller/club/clubController.js
@@ -1,68 +1,11 @@
 const baseResponse = require("../../config/baseResponse");
 
-const { clubRequest } = require("../../service/club/postService");
-const { clubSearch, clubMember, clubDetail, userBelong } = require("../../provider/club/postProvider");
-/*
- * API No. 2-9
- * API Name : 책 모임 가입 요청 전송 API
- * [POST] /request
- */
-module.exports.clubRequest = async (req, res) => {
-  try {
-    const club_id = parseInt(req.params.club_id);
-    const user_id = parseInt(req.body.user_id);
-
-    if (!user_id) {
-      res.send(response(baseResponse.USER_USERID_EMPTY));
-    } else if (!club_id){
-        res.send("This is not proper club_id");
-    } else {
-      const RequestResponse = await clubRequest(
-        user_id,
-        club_id,
-      );
-      console.log("가입 요청 성공하였습니다.");
-      //return res.send(RequestResponse);
-      return res.status(200).json({ message: '가입 요청 성공하였습니다. ! '});
-    }
-  } catch (err) {
-    console.log("Error 입니다.", err);
-    //오류 메시지 반환.
-    return res.status(res.statusCode).send(err._message);
-  }
-};
+// const { clubRequest } = require("../../service/club/clubService");
+const { clubSearch, clubMember, clubDetail, userBelong } = require("../../provider/club/clubProvider");
 
 
 /*
- * API No. 2 - 10
- * API Name : 책 모임 조회 API
- * [GET] /request
- */
-module.exports.clubSearch = async (req, res) => {
-  try {
-    const { club_id } = req.params;
-
-    //const { user_id } = req.body.user_id; 없어도 되나요?
-
-    if (!club_id){
-        res.send("This is not proper club_id");
-    } else {
-      const SearchResponse = await clubSearch(
-        club_id,
-      );
-      console.log(SearchResponse);
-      return res.send(SearchResponse);
-    }
-  } catch (err) {
-    console.log("Error", err);
-    //오류 메시지 반환.
-    return res.status(res.statusCode).send(err._message);
-  }
-};
-
-
-/*
- * API No. 3
+ * API No. 2-1
  * API Name : 책 모임 소속 회원 정보 조회 API
  * [GET] /member
  */
@@ -116,7 +59,7 @@ module.exports.clubDetail = async (req, res) => {
   
 
   /*
- * API No. 5
+ * API No. 2-5
  * API Name : 사용자(userid)가 속한 책모임을 조회함
  * [GET] /user/:user_id
  */
@@ -139,3 +82,62 @@ module.exports.userBelong = async (req, res) => {
       return res.status(res.statusCode).send(err._message);
     }
   };
+
+
+/*
+ * API No. 2-9
+ * API Name : 책 모임 가입 요청 전송 API
+ * [POST] /request
+ */
+// module.exports.clubRequest = async (req, res) => {
+//   try {
+//     const club_id = parseInt(req.params.club_id);
+//     const user_id = parseInt(req.body.user_id);
+
+//     if (!user_id) {
+//       res.send(response(baseResponse.USER_USERID_EMPTY));
+//     } else if (!club_id){
+//         res.send("This is not proper club_id");
+//     } else {
+//       const RequestResponse = await clubRequest(
+//         user_id,
+//         club_id,
+//       );
+//       console.log("가입 요청 성공하였습니다.");
+//       //return res.send(RequestResponse);
+//       return res.status(200).json({ message: '가입 요청 성공하였습니다. ! '});
+//     }
+//   } catch (err) {
+//     console.log("Error 입니다.", err);
+//     //오류 메시지 반환.
+//     return res.status(res.statusCode).send(err._message);
+//   }
+// };
+
+
+/*
+ * API No. 2 - 10
+ * API Name : 책 모임 조회 API
+ * [GET] /request
+ */
+module.exports.clubSearch = async (req, res) => {
+  try {
+    const { club_id } = req.params;
+
+    //const { user_id } = req.body.user_id; 없어도 되나요?
+
+    if (!club_id){
+        res.send("This is not proper club_id");
+    } else {
+      const SearchResponse = await clubSearch(
+        club_id,
+      );
+      console.log(SearchResponse);
+      return res.send(SearchResponse);
+    }
+  } catch (err) {
+    console.log("Error", err);
+    //오류 메시지 반환.
+    return res.status(res.statusCode).send(err._message);
+  }
+};

ba49bb6b7b6d4d48698f2694ca7cf7a871a095af
Dawon00 <da6921@naver.com>
2023-02-09 22:54:19 +0900

    fix: 게시물 작성 일자 기능 추가

--- router/club/postRoute.js
+++ router/club/postRoute.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const postController = require("../../controller/club/postController");
-// 진철님 코드와 통일
+// 진철님 aws s3 이미지 업로드 코드와 통일할 경우
 // const { imgUpload } = require("../../utils/middelware");
-const { imgUpload } = require("../../utils/imageUploader");
+const imgUpload = require("../../utils/imageUploader");
 
 router.route("/like/:club_post_id").post(postController.changeLike);
 router.route("/register/notice/:club_id").post(postController.announcement);

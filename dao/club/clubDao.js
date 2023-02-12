9908169901843bc9fdf1dbbc01027bc77b5d9722
E ON <ykiki5778@inha.edu>
2023-02-13 01:31:40 +0900

    fix: clubDao.js

--- dao/club/clubDao.js
+++ dao/club/clubDao.js
@@ -117,7 +117,7 @@ module.exports.PostDetail = async (connection, [club_id]) =>{
 
 // 2 - 5 책 모임의 소속 회원의 이름, 상태메시지, 현재 상태를 조회함
 module.exports.clubMember = async (connection, [club_id]) =>{
-    const clubSearchQuery =`SELECT UserInfo.user_id, UserInfo.now_reading, UserInfo.profile_message FROM UserInfo LEFT JOIN ClubMember ON UserInfo.user_id = ClubMember.user_id LEFT JOIN ClubInfo ON ClubMember.club_id = ClubInfo.club_id WHERE ClubInfo.club_id = ?;`;
+    const clubSearchQuery =`SELECT UserInfo.user_id, UserInfo.now_reading, UserInfo.introduce_message FROM UserInfo LEFT JOIN ClubMember ON UserInfo.user_id = ClubMember.user_id LEFT JOIN ClubInfo ON ClubMember.club_id = ClubInfo.club_id WHERE ClubInfo.club_id = ?;`;
     const clubSearchRow = await connection.query(clubSearchQuery, [club_id]);
     
     return clubSearchRow[0];

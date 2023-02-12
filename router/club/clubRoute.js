161165918cc0ef0b0c6af7b11ca065861f103584
Dawon00 <da6921@naver.com>
2023-02-13 00:59:21 +0900

    fix: 클럽생성, 클럽설정수정 api 수정

--- router/club/clubRoute.js
+++ router/club/clubRoute.js
@@ -1,30 +1,31 @@
-
 const express = require("express");
 const multer = require("multer");
 const router = express.Router();
-const { findClubSetting, 
-        postEditClubSetting, 
-        postNewClub, 
-        findRequestingMembers, 
-        approvalToJoinRequest,
-        declineToJoinRequest,
-        postJoinRequest } = require('../../controller/club/clubController');
-const { imgUpload } = require('../../utils/middelware');
-const { redisGet } = require('../../config/redis');
+const {
+  findClubSetting,
+  postEditClubSetting,
+  postNewClub,
+  findRequestingMembers,
+  approvalToJoinRequest,
+  declineToJoinRequest,
+  postJoinRequest,
+} = require("../../controller/club/clubController");
+const imgUpload = require("../../utils/imageUploader");
+const { redisGet } = require("../../config/redis");
 
 // JWT Verify.
 //const { authJWT } = require('../../config/auth');
 // Redis Cache.
 //const { get } = require('../../utils/cache');
 
-
-router.route('/setting/:club_id').get(redisGet, findClubSetting);
-router.route('/register').post(imgUpload.single("club_img_url"), postNewClub);
-router.route('/setting/edit/:club_id').post(imgUpload.single("club_img_url"), postEditClubSetting)
-router.route('/members/request/:club_id').get(findRequestingMembers);
-router.route('/request/:club_id').post(postJoinRequest)
-router.route('/members/approval/:user_id').post(approvalToJoinRequest)
-router.route('/members/decline/:user_id').delete(declineToJoinRequest)
-
+router.route("/setting/:club_id").get(redisGet, findClubSetting);
+router.route("/register").post(imgUpload.single("club_img_url"), postNewClub);
+router
+  .route("/setting/edit/:club_id")
+  .post(imgUpload.single("club_img_url"), postEditClubSetting);
+router.route("/members/request/:club_id").get(findRequestingMembers);
+router.route("/request/:club_id").post(postJoinRequest);
+router.route("/members/approval/:user_id").post(approvalToJoinRequest);
+router.route("/members/decline/:user_id").delete(declineToJoinRequest);
 
 module.exports = router;

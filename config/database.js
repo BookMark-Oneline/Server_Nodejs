9c7c2ea1b1826fe4a57099389a6cf00c3a1a435e
Dawon00 <da6921@naver.com>
2023-02-12 16:06:54 +0900

    fix: DB 포트 번호

--- config/database.js
+++ config/database.js
@@ -5,8 +5,8 @@ const mysql = require("mysql2/promise");
 const config = {
   host: `${process.env.DB_HOST}`,
   user: `${process.env.DB_USER}`,
-  // port: `32405`,
-  port: "3306",
+  port: `32405`,
+  // port: "3306",
   password: `${process.env.DB_PASS}`,
   database: `${process.env.DB_NAME}`,
 };

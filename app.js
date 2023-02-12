995a6117694d80e9b60fe556e3de8af7afef514b
Jin Cheol <76617139+bik1111@users.noreply.github.com>
2023-02-12 18:11:17 +0900

    Update app.js

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
     secret: "Hello!",
     sotre: new MySQLStore({
       host: `${process.env.DB_HOST}`,
-      port: 32405,
+      port: 3307,
       user: `${process.env.DB_USER}`,
       password: `${process.env.DB_PASS}`,
       database: `${process.env.DB_NAME}`,

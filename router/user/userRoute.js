fa695e9aa612045a8266b57ad729cceac9957ee5
Jin Cheol <76617139+bik1111@users.noreply.github.com>
2023-02-13 18:57:25 +0900

    Update userRoute.js

--- router/user/userRoute.js
+++ router/user/userRoute.js
@@ -1,18 +1,17 @@
 const express = require("express");
 const router = express.Router();
-// const {
-//   postLogin,
-//   postRegister,
-//   authAppleLogin,
-//   logout,
-// } = require("../../controller/user/userController");
-// const  refresh  = require('../../config/refreshToken');
+const {
+   postLogin,
+   postRegister,
+   authAppleLogin,
+   logout, } = require("../../controller/user/userController");
+const  refresh  = require('../../config/refreshToken');
 
 // router.route('/login').post(postLogin);
 // router.route('/register').post(postRegister)
 // router.route('/logout').post(logout)
 // router.get('/refresh', refresh);
 
-// router.post('/apple/auth', authAppleLogin)
+router.post('/apple/auth', authAppleLogin)
 
 module.exports = router;

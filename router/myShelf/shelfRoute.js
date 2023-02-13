c0a279d7ea8334105a79753719ea402df2d59da6
Jin Cheol <76617139+bik1111@users.noreply.github.com>
2023-02-13 18:56:26 +0900

    Update shelfRoute.js

--- router/myShelf/shelfRoute.js
+++ router/myShelf/shelfRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const { findMyAllBooks, searchBookDetail } = require('../../controller/myShelf/shelfController');
-const { redisGet } = require('../../config/redis');
+//const { redisGet } = require('../../config/redis');
 
-router.route('/:user_id').get(redisGet, findMyAllBooks);
-router.route('/book/:book_id').get(redisGet, searchBookDetail);
+router.route('/:user_id').get(findMyAllBooks);
+router.route('/book/:book_id').get(searchBookDetail);
 
 module.exports = router; 
